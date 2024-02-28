a60caff80b82576d82deee56b46c5469b38f9e6e
lovef2eLia <lia.lovef2e@gmail.com>
2024-02-29 02:23:18 +0800

    feat: index and read more info

--- app.js
+++ app.js
@@ -5,7 +5,20 @@ const port = 3000;
 const RESTAURANTS = require('./public/jsons/restaurant.json').results;
 
 app.get('/', (req, res) => {
-  res.render('index');
+  res.redirect('/restaurants');
+});
+
+app.get('/restaurants', (req, res) => {
+  res.render('index', { restaurants: RESTAURANTS });
+});
+
+app.get('/restaurant/:id', (req, res) => {
+  const id = req.params.id;
+  const restaurant = RESTAURANTS.find(
+    (restaurant) => restaurant.id.toString() === id
+  );
+
+  res.render('moreInfo', { restaurant: restaurant });
 });
 
 app.set('views', './views');
