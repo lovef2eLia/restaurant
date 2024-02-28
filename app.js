60b3a37def6c8d705df50da9c1a16f0e1b458308
lovef2eLia <lia.lovef2e@gmail.com>
2024-02-29 03:00:31 +0800

    feat: search restaurant bar

--- app.js
+++ app.js
@@ -2,19 +2,24 @@ const express = require('express');
 const { engine } = require('express-handlebars');
 const app = express();
 const port = 3000;
-const RESTAURANTS = require('./public/jsons/restaurant.json').results;
+const restaurants = require('./public/jsons/restaurant.json').results;
 
 app.get('/', (req, res) => {
   res.redirect('/restaurants');
 });
 
 app.get('/restaurants', (req, res) => {
-  res.render('index', { restaurants: RESTAURANTS });
+  const keyword = req.query.search?.trim();
+  const matchedRestaurants = keyword ? restaurants.filter((restaurant) => {
+    return restaurant.name.toLowerCase().includes(keyword.toLowerCase());
+  }) : restaurants;
+
+  res.render('index', { restaurants: matchedRestaurants });
 });
 
 app.get('/restaurant/:id', (req, res) => {
   const id = req.params.id;
-  const restaurant = RESTAURANTS.find(
+  const restaurant = restaurants.find(
     (restaurant) => restaurant.id.toString() === id
   );
 
